27b8c988be11b713ce301e3af695e51d19c2c407
Arjun Krishnan <akrish29@ucsc.edu>
2023-12-06 12:54:04 -0800

    Updated gumball anims + overworld tilemap and collision size changes

--- src/Scenes/Load.js
+++ src/Scenes/Load.js
@@ -23,11 +23,15 @@ class Load extends Phaser.Scene {
         this.load.image('enemy', './assets/Sprites/enemy.png')
         this.load.image('tilesetImage', './assets/tilemaps/gumball_tileset.png')
         this.load.tilemapTiledJSON('tilemapJSON', './assets/tilemaps/Overworld.json')
-        this.load.image('gumball', './assets/Prefabs_ANIM_Frames/Gumball.png')
         this.load.spritesheet('everythingStore', './assets/Prefabs_ANIM_Frames/EVERYTHINGSTORE.png', {
             frameWidth: 425,
             frameHeight: 500,
         })
+        this.load.spritesheet('gumball', './assets/Prefabs_ANIM_Frames/Gumball.png', {
+            frameWidth: 75,
+            frameHeight: 100,
+        })
+
         // load audio assets
         this.load.path = './assets/Sounds/'
         this.load.audio('hit', 'hit.wav')
@@ -46,6 +50,96 @@ class Load extends Phaser.Scene {
                 frames: [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4]
             })
         })
+
+        // overworld character anims
+
+        // Idle up
+        this.anims.create({
+            key: 'idle-up',
+            frameRate: 0,
+            repeat: -1,
+            frames: this.anims.generateFrameNumbers('gumball', {
+                start: 2,
+                end: 2
+            })
+        })
+
+        // Idle down
+        this.anims.create({
+            key: 'idle-down',
+            frameRate: 0,
+            repeat: -1,
+            frames: this.anims.generateFrameNumbers('gumball', {
+                start: 3,
+                end: 3
+            })
+        })
+
+        // Idle left
+        this.anims.create({
+            key: 'idle-left',
+            frameRate: 0,
+            repeat: -1,
+            frames: this.anims.generateFrameNumbers('gumball', {
+                start: 2,
+                end: 2
+            })
+        })
+
+        // Idle right
+        this.anims.create({
+            key: 'idle-right',
+            frameRate: 0,
+            repeat: -1,
+            frames: this.anims.generateFrameNumbers('gumball', {
+                start: 3,
+                end: 3
+            })
+        })
+
+        // Walk up
+        this.anims.create({
+            key: 'walk-up',
+            frameRate: 5,
+            repeat: -1,
+            frames: this.anims.generateFrameNumbers('gumball', {
+                start: 4,
+                end: 5
+            })
+        });
+
+        // Walk down
+        this.anims.create({
+            key: 'walk-down',
+            frameRate: 5,
+            repeat: -1,
+            frames: this.anims.generateFrameNumbers('gumball', {
+                start: 1,
+                end: 0
+            })
+        });
+
+        // Walk left
+        this.anims.create({
+            key: 'walk-left',
+            frameRate: 5,
+            repeat: -1,
+            frames: this.anims.generateFrameNumbers('gumball', {
+                start: 1,
+                end: 0
+            })
+        });
+
+        // Walk right
+        this.anims.create({
+            key: 'walk-right',
+            frameRate: 5,
+            repeat: -1,
+            frames: this.anims.generateFrameNumbers('gumball', {
+                start: 4,
+                end: 5
+            })
+        });
 
         // go to Title scene
         this.scene.start('menuScene');
