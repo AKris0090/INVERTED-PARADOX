3fffd76d9ec1b6a8b890736e09fd1a52d4533ca8
proudnhello <92887967+proudnhello@users.noreply.github.com>
2023-12-11 10:42:28 -0800

    slowed down player attack animation

--- src/Scenes/Load.js
+++ src/Scenes/Load.js
@@ -228,7 +228,7 @@ class Load extends Phaser.Scene {
         // Gumball ATTACK
         this.anims.create({
             key: 'gumAttack',
-            frameRate: 40,
+            frameRate: 25,
             repeat: 0,
             frames: this.anims.generateFrameNumbers('gumBattle', {
                 frames: [6, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 11, 11, 11]
