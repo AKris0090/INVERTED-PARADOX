8de32c887d2db6a5a38b7ba0f382a0737408b8d5
proudnhello <92887967+proudnhello@users.noreply.github.com>
2023-12-14 16:08:48 -0800

    added instruction text, expanded main comment

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 // Created by Arjun Krishnan and Moore Macauley
-// Sound effects made with https://sfxr.me/
-// GRASS TEXTURE PACK FOR BATTLE SCENE: https://opengameart.org/content/grass-texture-pack
 // For our components, we use:
 // Physics, for the movement of the character in the overworld
 // Tilemap, for creating the map in the overworld
@@ -15,8 +13,14 @@
 // Then the boss can usually be beaten around level 10
 // This is based on my own testing, stat increases are random, so you may get lucky or not
 // But this seems to be on average
+// You can press 1 to strat a random encounter on the overworld, and 2 to fight the boss
+// This will cause multiple music tracks to loop over each other, and the gumball may not end the random enounter where he started it
+// This is due to this feature being lazily implemented for testing purposes only, but we've left it in just in case you want to make your playthrough
+// a bit quicker and a lot louder
+// On a similar note, you can press shift on the game over textbox in battle to return you to the overworld instead of the title screen.
 
 let config = {
+    parent:"phaser-game",
     type: Phaser.AUTO,
     width: 1200,
     height: 600,
